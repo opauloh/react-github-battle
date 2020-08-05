c194beee416054b0dae31d86255522935cc9d254
opauloh <paulo.scape@gmail.com>
2020-08-04 22:35:34 -0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  // React Element
+  // Where to render to the element to
+  <App />,
+  document.getElementById('app')
+)
