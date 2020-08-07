75f3a80cff509b4ccb916305a28337c52b00bee5
opauloh <paulo.scape@gmail.com>
2020-08-07 13:04:49 -0300

    split Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,40 @@
 import React from 'react';
 
-export default class Popular extends React.Component {
-  render() {
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 
-    return (
-      <ul className="flex-center">
-        {languages.map((language) => (
-          <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
-          </li>
-        ))}
-      </ul>
-    );
+  return (
+    <ul className="flex-center">
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            onClick={() => onUpdateLanguage(language)}
+            style={language === selected ? { color: 'rgb(187,46,31' } : null}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage });
+  }
+  render() {
+    const { selectedLanguage } = this.state;
+    return <LanguagesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />;
   }
 }
