2a8165e3d0a4cbc85949132eef917fd55417fc18
opauloh <paulo.scape@gmail.com>
2020-08-12 00:28:37 -0300

    Popular Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -24,6 +25,46 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
 LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
+};
+
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url} className="card bg-light">
+            <Card header={`#${index + 1}`} avatar={avatar_url} href={html_url} name={login}>
+              <ul className="card-list">
+                <li>
+                  <FaUser color="rgb(255, 191, 116)" size={22} />
+                  <a href={`https://github.com/${login}`}>{login}</a>
+                </li>
+                <li>
+                  <FaStar color="rgb(255, 215, 0)" size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                  {open_issues.toLocaleString()} open issues
+                </li>
+              </ul>
+            </Card>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired
 };
 
 export default class Popular extends React.Component {
@@ -79,8 +120,8 @@ export default class Popular extends React.Component {
       <React.Fragment>
         <LanguagesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
         {this.isLoading() && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+        {error && <p className="center-text error">{error}</p>}
+        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </React.Fragment>
     );
   }
