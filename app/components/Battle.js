0343b3380ebf0cdd105eb31a9a7548bc4b4b4c23
opauloh <paulo.scape@gmail.com>
2020-09-22 15:53:51 -0300

    get Data from url

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -3,6 +3,7 @@ import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icon
 import PropTypes from 'prop-types';
 import Results from './Results';
 import { ThemeConsumer } from '../contexts/theme';
+import { Link } from 'react-router-dom';
 
 function Instructions() {
   return (
@@ -127,8 +128,7 @@ export default class Battle extends React.Component {
 
     this.state = {
       playerOne: null,
-      playerTwo: null,
-      battle: null
+      playerTwo: null
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -147,23 +147,7 @@ export default class Battle extends React.Component {
     });
   }
   render() {
-    const { playerOne, playerTwo, battle } = this.state;
-
-    if (battle) {
-      return (
-        <Results
-          playerOne={playerOne}
-          playerTwo={playerTwo}
-          onReset={() =>
-            this.setState({
-              playerOne: null,
-              playerTwo: null,
-              battle: null
-            })
-          }
-        />
-      );
-    }
+    const { playerOne, playerTwo } = this.state;
 
     return (
       <React.Fragment>
@@ -198,11 +182,14 @@ export default class Battle extends React.Component {
           </div>
 
           {playerOne && playerTwo && (
-            <button
+            <Link
               className="btn dark-btn btn-space"
-              onClick={() => this.setState({ battle: true })}>
+              to={{
+                pathname: '/battle/results',
+                search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+              }}>
               Battle
-            </button>
+            </Link>
           )}
         </div>
       </React.Fragment>
